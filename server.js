7fc2850224352b084c2e1b2f2b6f80275cdb64e9
knitesh5 <knitesh0596@gmail.com>
2017-11-06 16:30:23 +0530

    checking push

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
 
-
 app.use((req,res,next)=>{
 	var now = new Date().toString();
 	var log = `${now}: ${req.method} ${req.url}`;
